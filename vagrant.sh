96d598fe52a1dfc20992fda56919e6c99472bfe7
Aaron Weiss <aaronweiss74@gmail.com>
2017-01-29 16:54:11 -0500

    Added Oracle Java 8 installation to vagrant.sh.

--- vagrant.sh
+++ vagrant.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install curl apt-transport-https ca-certificates
+sudo apt-get install curl apt-transport-https ca-certificates python-software-properties
 curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
 sudo add-apt-repository \
      "deb https://apt.dockerproject.org/repo/ \
        ubuntu-$(lsb_release -cs) \
        main"
+sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install -y docker-engine
+sudo apt-get install -y docker-engine oracle-java8-installer
