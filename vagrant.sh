48958f115679ca070b9c6d3d050f0c3b1b3380d2
Aaron Weiss <aaronweiss74@gmail.com>
2017-01-29 16:17:12 -0500

    Set up Vagrant for Ubuntu 16.04.

---
+++ vagrant.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install curl apt-transport-https ca-certificates
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+sudo add-apt-repository \
+     "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
+sudo apt-get update
+sudo apt-get install -y docker-engine
