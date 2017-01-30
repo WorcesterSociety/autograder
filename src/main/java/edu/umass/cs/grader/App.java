90dc6569eab713a4327e0622dad05a76c5ac7571
Aaron Weiss <aaronweiss74@gmail.com>
2017-01-29 22:56:20 -0500

    Implemented broken example of running Python in Docker.

--- src/main/java/edu/umass/cs/grader/App.java
+++ src/main/java/edu/umass/cs/grader/App.java
@@ -17,8 +17,58 @@
  */
 package edu.umass.cs.grader;
 
+import com.spotify.docker.client.DefaultDockerClient;
+import com.spotify.docker.client.DockerClient;
+import com.spotify.docker.client.LogStream;
+import com.spotify.docker.client.exceptions.DockerCertificateException;
+import com.spotify.docker.client.exceptions.DockerException;
+import com.spotify.docker.client.messages.ContainerConfig;
+import com.spotify.docker.client.messages.ContainerCreation;
+import com.spotify.docker.client.messages.ExecCreation;
+import com.spotify.docker.client.messages.HostConfig;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            DockerClient docker = new DefaultDockerClient("unix:///var/run/docker.sock");
+
+            HostConfig hostConfig = HostConfig.builder()
+                    .appendBinds(HostConfig.Bind
+                            .from(".")
+                            .to("/opt/assignment")
+                            .readOnly(true)
+                            .build()
+                    )
+                    .build();
+
+            ContainerConfig config = ContainerConfig.builder().image("python:latest")
+                    .attachStdout(true).attachStderr(true)
+                    .tty(true)
+                    .env("PYTHONUNBUFFERED=0")
+                    .cmd("python", "-u", "-c", "'print(\"Hello, World.\")'")
+                    .build();
+
+            ContainerCreation container = docker.createContainer(config);
+            docker.startContainer(container.id());
+
+//            ExecCreation exec = docker.execCreate(container.id(), new String[]{
+//                    "python", "-c", "'print(\"Hello World\")'"
+//            }, DockerClient.ExecCreateParam.attachStdout(true), DockerClient.ExecCreateParam.attachStderr(true));
+//            LogStream stream = docker.execStart(exec.id(), DockerClient.ExecStartParameter.TTY);
+            docker.waitContainer(container.id());
+
+            String output = docker.logs(
+                    container.id(), DockerClient.LogsParam.stdout(), DockerClient.LogsParam.stderr()
+            ).readFully();
+
+            System.out.println("Python Output:");
+            System.out.println(output);
+
+            docker.removeContainer(container.id());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (DockerException e) {
+            e.printStackTrace();
+        }
     }
 }
