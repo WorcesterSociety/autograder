33e7f3d75289699f6da398e045f7e39adcba2f6c
Aaron Weiss <aaronweiss74@gmail.com>
2017-02-20 19:56:13 -0500

    Fixed MoodleUnzipper tests.

--- tests/test_moodle_unzipper.py
+++ tests/test_moodle_unzipper.py
@@ -2,16 +2,18 @@ from grader.moodle_unzipper import *
 import pytest
 
 not_macos_data = [
-    ("foo/bar/__MACOSX",  False),
-    ("a/b/blah.py",        True),
-    ("blah/__MACOSX/blah", True),
-    ("/foo/bar/baz",       True),
-    ("blah/MACOSX",        True),
+    ("foo/bar/__MACOSX",   False),
+    ("a/b/blah.py",         True),
+    ("blah/__MACOSX/blah", False),
+    ("/foo/bar/baz",        True),
+    ("blah/MACOSX",         True),
+    ("/.idea",             False),
+    ("__pycache__",        False),
 ]
 
 @pytest.mark.parametrize("path, expected", not_macos_data)
-def test_is_not_macos(path, expected):
-    assert is_not_macos(path) == expected
+def test_is_valid_path(path, expected):
+    assert is_valid_path(path) == expected
 
 
 flatten_data = [
